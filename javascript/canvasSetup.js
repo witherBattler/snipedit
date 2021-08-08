4b824bf05de0260361bd615309f6c68ac5db8fec
IMPixel <asemenov.ca@gmail.com>
2021-08-08 08:08:52 -0400

    save #1
    I don't want to lose files

---
+++ javascript/canvasSetup.js
@@ -0,0 +1,60 @@
+var elementsChain = []
+var initialImageScaleModifier
+var coefficientRatio
+var pasted = false
+var initialImage
+var using = "none";
+
+function retrieveImageFromClipboardAsBlob(pasteEvent, callback){
+	if(pasteEvent.clipboardData == false){
+        if(typeof(callback) == "function"){
+            callback(undefined);
+        }
+    };
+
+    var items = pasteEvent.clipboardData.items;
+
+    if(items == undefined){
+        if(typeof(callback) == "function"){
+            callback(undefined);
+        }
+    };
+
+    for (var i = 0; i < items.length; i++) {
+        if (items[i].type.indexOf("image") == -1) continue;
+        var blob = items[i].getAsFile();
+
+        if(typeof(callback) == "function"){
+            callback(blob);
+        }
+    }
+}
+
+window.addEventListener("paste", function(e){
+    if(!pasted){
+        retrieveImageFromClipboardAsBlob(e, function(imageBlob){
+            if(imageBlob){
+                var URLObj = window.URL || window.webkitURL;
+                src = URLObj.createObjectURL(imageBlob);
+                elementsChain.push({type: "initial image", src: src});
+                initialImage = loadImage(elementsChain[0].src, function() {
+                    pasted = true;
+                    coefficientRatio = Math.min(width / initialImage.width, height / initialImage.height);
+                })
+            }
+        });
+    }
+}, false);
+
+function setup() {
+    var canvas = createCanvas(900, 500)
+    canvas.style("border", "black 1px solid")
+}
+
+function draw() {
+    if(pasted) {
+        image(initialImage, 0, 0, initialImage.width * coefficientRatio, initialImage.height * coefficientRatio);
+    }
+}
+
+
---
+++ javascript/drawing.js
@@ -0,0 +1,10 @@
+function setup() {
+    var canvas = createCanvas(900, 500)
+    canvas.style("border", "black 1px solid")
+}
+
+function draw() {
+    if(pasted) {
+        image(initialImage, 0, 0, initialImage.width * coefficientRatio, initialImage.height * coefficientRatio);
+    }
+}
